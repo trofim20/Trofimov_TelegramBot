12145913c4aa5fa2094ed4f3030325f2e82701b9
ntrofim20 <ntrofim35@mail.ru>
2025-03-17 16:27:24 +0500

    Initial commit

---
+++ src/main/java/ru/trofimov/Trofimov_TelegramBot/TrofimovTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package ru.trofimov.Trofimov_TelegramBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrofimovTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrofimovTelegramBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/trofimov/Trofimov_TelegramBot/TrofimovTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.trofimov.Trofimov_TelegramBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrofimovTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
